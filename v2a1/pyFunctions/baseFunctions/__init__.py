b49a7df6cd181cb7edd57989c673d9f106bcfd90
Acedia <110048632+slothyace@users.noreply.github.com>
2024-10-09 22:43:43 +0800

    Delete v2a1 directory

--- v2a1/pyFunctions/baseFunctions/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import importlib
-
-packageDir = os.path.dirname(__file__)
-
-for moduleName in os.listdir(packageDir):
-    if moduleName.endswith('.py') and moduleName != "__init__.py":
-        moduleName = moduleName.strip(".py")
-        importlib.import_module(f".{moduleName}", package=__name__)
